0b556dcc88659ff14a9d2a61c377b1042e130e5b
Marcelo Morais <m2marcelo@gmail.com>
2021-06-02 12:03:55 +0200

    Added API key for civic api

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsHttpClient.kt
@@ -1,29 +1,36 @@
 package com.example.android.politicalpreparedness.network
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 
 class CivicsHttpClient: OkHttpClient() {
 
     companion object {
 
-        const val API_KEY = "" //TODO: Place your API Key Here
+        const val API_KEY = "AIzaSyCUagaAfSwYUwwirASGMmxjapJVPTVAVQ0"
+
+        val interceptor = HttpLoggingInterceptor()
+            .apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
 
         fun getClient(): OkHttpClient {
             return Builder()
-                    .addInterceptor { chain ->
-                        val original = chain.request()
-                        val url = original
-                                .url()
-                                .newBuilder()
-                                .addQueryParameter("key", API_KEY)
-                                .build()
-                        val request = original
-                                .newBuilder()
-                                .url(url)
-                                .build()
-                        chain.proceed(request)
-                    }
-                    .build()
+                .addInterceptor { chain ->
+                    val original = chain.request()
+                    val url = original
+                        .url
+                        .newBuilder()
+                        .addQueryParameter("key", API_KEY)
+                        .build()
+                    val request = original
+                        .newBuilder()
+                        .url(url)
+                        .build()
+                    chain.proceed(request)
+                }.addInterceptor(interceptor)
+                .build()
         }
 
     }
